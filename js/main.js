5f03d808f2beb4e2e1a35268de0c1a5b39f15322
tkhstzm <takahashi.tsuzumi@gmail.com>
2026-02-11 21:12:06 +0900

    css修正

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
 const links = document.querySelectorAll('.testimonial__link');
-const panels = document.querySelectorAll('.comment');
+const comments = document.querySelectorAll('.comment');
 
+// link部分のそれぞれにクリックイベントを設定
 links.forEach(link => {
   link.addEventListener('click', () => {
 
+    // クリックされた要素のdata-idを取得
     const id = link.dataset.id;
 
     // 左のactive切り替え
+    // activeを消して、クリックされたものにactiveをつける
     links.forEach(l => l.classList.remove('testimonial__link-active'));
     link.classList.add('testimonial__link-active');
 
     // 右の表示切り替え
-    panels.forEach(panel => {
-      panel.classList.remove('is-active');
-      if (panel.dataset.id === id) {
-        panel.classList.add('is-active');
+    // is-activeを消して、data-idが一致したものにis-activeをつける
+    comments.forEach(comment => {
+      comment.classList.remove('is-active');
+      if (comment.dataset.id === id) {
+        comment.classList.add('is-active');
       }
     });
-
   });
 });
