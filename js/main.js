0c5056f950b1ea74ef84b96eb26237d366bd3c49
tkhstzm <takahashi.tsuzumi@gmail.com>
2026-02-12 10:47:20 +0900

    クラス名の修正

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
-const links = document.querySelectorAll('.testimonial__link');
-const comments = document.querySelectorAll('.comment');
+const tabs = document.querySelectorAll('.testimonial-tab');
+const cards = document.querySelectorAll('.testimonial-card');
 
 // link部分のそれぞれにクリックイベントを設定
-links.forEach(link => {
-  link.addEventListener('click', () => {
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
 
     // クリックされた要素のdata-idを取得
-    const id = link.dataset.id;
+    const id = tab.dataset.id;
 
     // 左のactive切り替え
     // activeを消して、クリックされたものにactiveをつける
-    links.forEach(l => l.classList.remove('testimonial__link-active'));
-    link.classList.add('testimonial__link-active');
+    tabs.forEach(l => l.classList.remove('testimonial-tab--active'));
+    tab.classList.add('testimonial-tab--active');
 
     // 右の表示切り替え
     // is-activeを消して、data-idが一致したものにis-activeをつける
-    comments.forEach(comment => {
-      comment.classList.remove('is-active');
-      if (comment.dataset.id === id) {
-        comment.classList.add('is-active');
+    cards.forEach(card => {
+      card.classList.remove('is-active');
+      if (card.dataset.id === id) {
+        card.classList.add('is-active');
       }
     });
   });
